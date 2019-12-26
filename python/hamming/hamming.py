4887f1c0c580b4130f9baed207c37b04697803a0
bLeDy <deejaynof@gmail.com>
2019-12-26 19:17:22 +0100

    Update .gitignore

--- python/hamming/hamming.py
+++
@@ -1,7 +0,0 @@
-def distance(strand_a, strand_b):
-
-    if len(strand_a) == len(strand_b):
-        return sum(1 for i in range(len(strand_a)) if strand_a[i] != strand_b[i])
-
-    else:
-        raise ValueError("Strands provided are not the same length")
