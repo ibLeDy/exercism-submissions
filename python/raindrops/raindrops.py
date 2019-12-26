92e9934a97d836f44aa8c45e6096de96e4a93a55
bLeDy <deejaynof@gmail.com>
2019-12-26 19:17:22 +0100

    Update .gitignore

--- python/raindrops/raindrops.py
+++
@@ -1,7 +0,0 @@
-factors = ((3, "Pling"), (5, "Plang"), (7, "Plong"))
-
-def raindrops(number):
-
-    result = [s for f, s in factors if number % f == 0]
-
-    return "".join(result) if result else str(number)
--- python/two-fer/two_fer.py
+++
@@ -1,6 +0,0 @@
-def two_fer(name=""):
-    if name != "":
-        sentence = ("One for {}, one for me.".format(name))
-    else:
-        sentence = ("One for you, one for me.".format(name))
-    return sentence
