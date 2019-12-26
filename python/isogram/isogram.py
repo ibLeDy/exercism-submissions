29e3461c46c197679fe38bd029a100e461d05b73
bLeDy <deejaynof@gmail.com>
2019-12-26 19:18:41 +0100

    Remove cached

---
+++ python/isogram/isogram.py
@@ -0,0 +1,21 @@
+import string
+
+def is_isogram(text):
+
+    if not isinstance(text, str):
+        return False
+    elif text == "":
+        return True
+    elif len(text) < 1:
+        return False
+
+    punc = string.punctuation
+    s = list(text.lower())
+    
+    word = ''.join([o for o in s if not o in punc])
+    word = ''.join(word.strip().split())
+    
+    if len(word) == len(set(word)):
+        return True
+    else:
+        return False
