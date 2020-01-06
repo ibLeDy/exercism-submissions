71770f318bae4a69ac5d357c7059348598c8c261
bLeDy <deejaynof@gmail.com>
2020-01-06 03:51:43 +0100

    Improve variable naming

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,4 +1,4 @@
-from string import ascii_uppercase as letters
+from string import ascii_uppercase
 
 
 def abbreviate(words):
@@ -8,7 +8,7 @@ def abbreviate(words):
     for word in splitted:
         for letter in word:
             letter = letter.upper()
-            if letter in letters:
+            if letter in ascii_uppercase:
                 acronym.append(letter)
                 break
 
